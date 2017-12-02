11de941015f2fc8a1a42549dfc49773c0c7e2b91
Colin Marsch <colinmarsch@gmail.com>
2017-12-02 16:59:10 -0500

    Initial Commit

---
+++ Macify/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Macify
+//
+//  Created by Colin Marsch on 2017-12-02.
+//  Copyright © 2017 Colin Marsch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
